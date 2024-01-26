3f3887368235c1896744ab6bf5b12cc4a6c55cca
Alexandre RAGUIN <alexandre.raguin49.com>
2024-01-26 19:49:26 +0100

    new file:   .gitignore 	new file:   app.js 	new file:   img/.DS_Store 	new file:   img/1920x1080 n2.png 	new file:   img/1920x1080 n3.png 	new file:   img/1920x1080.jpeg 	new file:   img/enconstruction.png 	new file:   img/excel.jpeg 	new file:   img/forge.png 	new file:   img/logotypes/.DS_Store 	new file:   img/logotypes/daher.png 	new file:   img/logotypes/white-logo.png 	new file:   img/mc.png 	new file:   img/of.png 	new file:   img/outlook.jpeg 	new file:   index.html 	new file:   style.css

---
+++ app.js
@@ -0,0 +1,37 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+	arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+        arrowParent.classList.toggle("showMenu");});}
+			
+		let sidebar = document.querySelector(".sidebar");
+		let sidebarBtn = document.querySelector(".bx-menu");
+        console.log(sidebarBtn);
+		sidebarBtn.addEventListener("click", ()=>{sidebar.classList.toggle("close");});
+
+
+function openPopup() {
+	document.getElementById("accueil-popup").style.display = "block";
+}
+		
+function closePopup() {
+	document.getElementById("accueil-popup").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez tous les éléments avec la classe "card"
+    const cards = document.querySelectorAll(".card");
+
+    // Ajoutez un gestionnaire d'événement de clic à chaque carte
+    cards.forEach((card) => {
+      card.addEventListener("click", function () {
+        // Récupérez le lien de l'attribut data-link
+        const link = card.getAttribute("data-link");
+
+        // Redirigez vers le lien
+        if (link) {
+          window.location.href = link;
+        }
+      });
+    });
+  });
